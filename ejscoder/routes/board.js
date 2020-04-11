940c55d6d723c770d480e7316fb764570865df07
caesiumy <mn0658@naver.com>
2020-04-11 20:10:22 +0900

    feat: Add board write

--- ejscoder/routes/board.js
+++ ejscoder/routes/board.js
@@ -20,4 +20,22 @@ router.get("/list/:page", (req, res, next) => {
   });
 });
 
+router.get("/write", (req, res, next) => {
+  res.render("write", { title: "글쓰기" });
+});
+
+router.post("/write", (req, res, next) => {
+  var name = req.body.name;
+  var title = req.body.title;
+  var content = req.body.content;
+  var passwd = req.body.password;
+  var datas = [name, title, content, passwd];
+
+  var sql =
+    "insert into board(name, title, content, regdate, modidate, passwd, hit) values (?, ?, ?, now(), now(), ?, 0)";
+  conn.query(sql, datas, (err, rows) => {
+    if (err) console.log("err", err);
+    res.redirect("/board/list");
+  });
+});
 module.exports = router;
